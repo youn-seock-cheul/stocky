0ad8ea6a38ff41c309eac2086ab284104af1cd1b
youn-seock-cheul <158433063+youn-seock-cheul@users.noreply.github.com>
2026-06-16 09:41:07 +0900

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -15,8 +15,8 @@ class MarketDataCollector:
         if os.path.exists(self.portfolio_file):
             with open(self.portfolio_file, 'r', encoding='utf-8') as f: self.my_portfolio = json.load(f)
         else:
-            self.my_portfolio = [{"Samsung": {"ticker": "005930.KS", "avg_price": 6568901, "deposit": 7384545}}
-                                 ,{"RISE AI Infra": {"ticker": "0101N0.KS", "avg_price": 43426200, "deposit": 43202755}}]
+            self.my_portfolio = [{"Samsung": {"ticker": "005930.KS", "avg_price": 299,927, "deposit": 7,384,545}}
+                                 ,{"RISE AI Infra": {"ticker": "0101N0.KS", "avg_price": 22,931, "deposit": 43,202,755}}]
 
     def get_recent_data(self, days=30):
         def fetch(target, is_p=False):
